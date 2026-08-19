1bb5c69a1290651e5021947d1fa5870606895b99
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-19 17:04:01 +0800

    修复：特性开关-bug02

--- upsert.go
+++ upsert.go
@@ -114,7 +114,7 @@ func fromFlagRules(in []flag.Rule) []Rule {
 		out = append(out, Rule{
 			Attr:   r.Attr,
 			Op:     RuleOp(r.Op),
-			Values: r.Values,
+			Values: append([]string(nil), r.Values...),
 		})
 	}
 	return out
